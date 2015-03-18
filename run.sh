61ba9672e14862751a9269178af2dcee2c074731
Andrew Zenk <azenk@umn.edu>
2015-03-18 15:08:23 -0500

    Added GANGLIA_TEMPLATE_NAME environment variable

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+
+if [ -n "${GANGLIA_TEMPLATE_NAME}" ]; then
+    sed -i "s/\$template_name = \"default\"/\$template_name = \"${GANGLIA_TEMPLATE_NAME}\"/g" /usr/share/ganglia/conf.php
+fi
 service gmetad start
 /usr/sbin/httpd -D FOREGROUND
