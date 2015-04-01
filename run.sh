336038a184254e5a9fddad065fd32a7c1402e85e
Andrew Zenk <azenk@umn.edu>
2015-04-01 16:41:37 -0500

    Added support for ganglia debug output

--- run.sh
+++ run.sh
@@ -4,5 +4,12 @@
 if [ -n "${GANGLIA_TEMPLATE_NAME}" ]; then
     sed -i "s/\$template_name = \"default\"/\$template_name = \"${GANGLIA_TEMPLATE_NAME}\"/g" /usr/share/ganglia/conf.php
 fi
-service gmetad start
-/usr/sbin/httpd -D FOREGROUND
+
+
+if [ -n "${GANGLIA_DEBUG}" ]; then
+    service httpd start
+    /usr/sbin/gmetad --debug=${GANGLIA_DEBUG}
+else
+    service gmetad start
+    /usr/sbin/httpd -D FOREGROUND
+fi
